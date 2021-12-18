82dc7830284ac06b5bc470b8ef82f86f8064674b
Karl Jose Buena <karljosebuena@gmail.com>
2021-12-18 11:21:15 +0800

    Added eslint, prettier, postgres, docker, prisma.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async () => {
+  // debugger;
+
+  return { hello: "Hello docker compose-postgres!" };
+});
+
+const port = process.env.PORT ?? 8080;
+const start = async () => {
+  try {
+    await fastify.listen(port, '0.0.0.0');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
